c85272833af619e81ca184fcb1b8a02e48ded482
chanryc9471 <91750491+chanryc9471@users.noreply.github.com>
2023-04-12 23:52:41 -0400

    update

--- src/pages/NutritionalRecipes/index.tsx
+++ src/pages/NutritionalRecipes/index.tsx
@@ -173,25 +173,28 @@ export default function Simple() {
         Nutritional Recipes
       </Text>
       
-
+      <Flex direction="row">
+{/*Sweet Potato kale shrimp skillet */}
       <Box ml={15} >
       <Card maxW='sm' >
   <CardBody >
     <Image 
-      src='https://www.shape.com/thmb/rcoAEWTEu6i6zEvZKfU3DkcyA5c=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/800_SweetpotatoKaleShrimpskillet_PrimaveraKitchen-600px-e1573579470176-7d1a0934aa1145ccbc4e092e419c5e32.jpg'
+      src='https://www.primaverakitchen.com/wp-content/uploads/2018/06/Sweet-potato-Kale-and-Shrimp-Skillet-Primavera-Kitchen-1-1.jpg'
       alt='Sweet Potatoes, Kale, and Shrimp in a black bowl with a wooden spoon'
       borderRadius='lg'
+      
+      height="230"
     />
-    <Stack my='6' spacing='5'>
+    <Stack my='6' spacing='2'>
       <Heading size='md'>Sweet Potato, Kale, and Shrimp Skillet</Heading>
       <Text>
       This gluten-free recipe uses sweet potato, kale, and shrimp to make a quick high-protein weekday meal. Even better: It only uses one pan!
       </Text>
-      <Text ml='8' color='orange.600' fontSize='2xl' size = 'md'>
+      <Text ml='8' color='red.300' fontSize='2xl' size = 'md'>
         325 - 460 calories
       </Text>
     </Stack>
-    <Button ml='7'variant='solid' colorScheme='orange' onClick={onOpen} > {/*variant attribute can also hold ghost */}
+    <Button ml='7'variant='solid' colorScheme='red' onClick={onOpen} > {/*variant attribute can also hold ghost */}
              View Recipe            
 </Button>
 
@@ -206,19 +209,19 @@ export default function Simple() {
       <PopoverBody>
       <Text> Carbs - 18g
       <Stack spacing={5}>
-          <Progress colorScheme='blue' size='md' value={6} />
+          <Progress colorScheme='blue' size='md' value={31} />
       </Stack>
       </Text>
       <br></br>
       <Text> Protein - 32g
       <Stack spacing={5}>
-          <Progress colorScheme='red' size='md' value={64} />
+          <Progress colorScheme='red' size='md' value={55} />
       </Stack>
       </Text>
       <br></br>
       <Text> Fat - 8g
       <Stack spacing={5}>
-          <Progress colorScheme='yellow' size='md' value={12} />
+          <Progress colorScheme='yellow' size='md' value={14} />
       </Stack>
       </Text>        
 
@@ -267,38 +270,218 @@ export default function Simple() {
 
   </DrawerContent>
 </Drawer>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 </ButtonGroup>
 </Card>
       </Box>
+
+{/*Roasted Broccoli with Shrimp*/}
+      <Box ml={15} >
+      <Card maxW='sm' >
+  <CardBody >
+    <Image 
+      src='https://cdn.muscleandstrength.com/sites/default/files/images/articles/high-protein-roasted-shrimp-with-broccoli.jpg'
+      alt='Sweet Potatoes, Kale, and Shrimp in a black bowl with a wooden spoon'
+      borderRadius='lg'
+      width="300"
+      height="230"
+    />
+    <Stack my='6' spacing='2'>
+      <Heading size='md'>Roasted Broccoli with Shrimp</Heading>
+      <Text>
+      This nutrient-rich dinner only dirties one pan and comes together in just a few minutes. It's the perfect healthy, weeknight meal. Note: if you don't keep whole spices, you can use half the amount of ground spices instead. 
+      </Text>
+      <Text ml='8' color='red.300' fontSize='2xl' size = 'md'>
+        350 - 480 calories
+      </Text>
+    </Stack>
+    <Button ml='7'variant='solid' colorScheme='red' onClick={onOpen} > {/*variant attribute can also hold ghost */}
+             View Recipe            
+</Button>
+
+<Popover>
+  <PopoverTrigger>
+    <Button ml='7' >View Macros</Button>
+  </PopoverTrigger>
+  <PopoverContent>
+    <PopoverArrow />
+    <PopoverCloseButton />
+    <PopoverHeader><strong>Macro Chart</strong></PopoverHeader>
+      <PopoverBody>
+      <Text> Carbs - 12g
+      <Stack spacing={5}>
+          <Progress colorScheme='blue' size='md' value={21} />
+      </Stack>
+      </Text>
+      <br></br>
+      <Text> Protein - 29g
+      <Stack spacing={5}>
+          <Progress colorScheme='red' size='md' value={50} />
+      </Stack>
+      </Text>
+      <br></br>
+      <Text> Fat - 16g
+      <Stack spacing={5}>
+          <Progress colorScheme='yellow' size='md' value={29} />
+      </Stack>
+      </Text>        
+
+      </PopoverBody>
+  </PopoverContent>
+</Popover>
+
+  </CardBody>
+  <ButtonGroup spacing='2'>
+<Drawer
+  isOpen={isOpen}
+  placement='right'
+  onClose={onClose}
+  size = "md"
+   
+>
+  <DrawerOverlay />
+  <DrawerContent>
+  
+    <DrawerCloseButton />
+    <DrawerHeader mt='-2'fontSize="35" my='-3'>Roasted Broccoli with Shrimp</DrawerHeader>
+          
+    <DrawerBody>
+      
+    <Text fontSize="30" pb={2} >
+      Ingredients: </Text>
+    
+    <UnorderedList fontSize="18"  ml={19}>
+      <ListItem>2 pounds broccoli, cut into bite-size florets</ListItem>
+      <ListItem>4 tablespoons (¼ cup) extra virgin olive oil</ListItem>
+      <ListItem>1 teaspoon whole coriander seeds</ListItem>
+      <ListItem>1 teaspoon whole cumin seeds</ListItem>
+      <ListItem>1½ teaspoons kosher salt</ListItem>
+      <ListItem>1 teaspoon freshly ground black pepper</ListItem>
+      <ListItem>⅛ teaspoon hot chili powder</ListItem>
+      <ListItem>1 pound large shrimp, shelled and deveined</ListItem>
+      <ListItem>1¼ teaspoons lemon zest (from 1 large lemon)</ListItem>
+    </UnorderedList>
+
+     <Button mt={5}>
+     Add Ingredients to Grocery List      
+     </Button>
+     <br></br>
+     <br></br>
+     
+    </DrawerBody>
+
+  </DrawerContent>
+</Drawer>
+</ButtonGroup>
+</Card>
+      </Box>
+
+
+{/* Make-Ahead Breakfast Burritos*/}
+      <Box ml={15} >
+      <Card maxW='sm' >
+  <CardBody >
+    <Image 
+      src='https://skinnyms.com/wp-content/uploads/2014/05/Southwestern-Protein-Breakfast-Burrito-Breakfast-Recipe-2-1200x799.jpg'
+      alt='3 sliced breakfast burritos '
+      borderRadius='lg'
+      width="300"
+      height="230"
+    />
+    <Stack my='6' spacing='2'>
+      <Heading size='md'>Make-Ahead Breakfast Burritos</Heading>
+      <Text>
+      Although this is more template than recipe, make-ahead breakfast burritos are a convenient way to eat healthy on the go. These meal-prep friendly burritos are freezer-friendly and customizable. 
+      </Text>
+      <Text ml='8' color='red.300' fontSize='2xl' size = 'md'>
+        300 - 600 calories
+      </Text>
+    </Stack>
+    <Button ml='7'variant='solid' colorScheme='red' onClick={onOpen} > {/*variant attribute can also hold ghost */}
+             View Recipe            
+</Button>
+
+<Popover>
+  <PopoverTrigger>
+    <Button ml='7' >View Macros</Button>
+  </PopoverTrigger>
+  <PopoverContent>
+    <PopoverArrow />
+    <PopoverCloseButton />
+    <PopoverHeader><strong>Macro Chart</strong></PopoverHeader>
+      <PopoverBody>
+      <Text> Carbs - 12g
+      <Stack spacing={5}>
+          <Progress colorScheme='blue' size='md' value={21} />
+      </Stack>
+      </Text>
+      <br></br>
+      <Text> Protein - 29g
+      <Stack spacing={5}>
+          <Progress colorScheme='red' size='md' value={50} />
+      </Stack>
+      </Text>
+      <br></br>
+      <Text> Fat - 16g
+      <Stack spacing={5}>
+          <Progress colorScheme='yellow' size='md' value={29} />
+      </Stack>
+      </Text>        
+
+      </PopoverBody>
+  </PopoverContent>
+</Popover>
+
+  </CardBody>
+  <ButtonGroup spacing='2'>
+<Drawer
+  isOpen={isOpen}
+  placement='right'
+  onClose={onClose}
+  size = "md"
+   
+>
+  <DrawerOverlay />
+  <DrawerContent>
+  
+    <DrawerCloseButton />
+    <DrawerHeader mt='-2'fontSize="35" my='-3'>Roasted Broccoli with Shrimp</DrawerHeader>
+          
+    <DrawerBody>
+      
+    <Text fontSize="30" pb={2} >
+      Ingredients: </Text>
+    
+    <UnorderedList fontSize="18"  ml={19}>
+      <ListItem>2 pounds broccoli, cut into bite-size florets</ListItem>
+      <ListItem>4 tablespoons (¼ cup) extra virgin olive oil</ListItem>
+      <ListItem>1 teaspoon whole coriander seeds</ListItem>
+      <ListItem>1 teaspoon whole cumin seeds</ListItem>
+      <ListItem>1½ teaspoons kosher salt</ListItem>
+      <ListItem>1 teaspoon freshly ground black pepper</ListItem>
+      <ListItem>⅛ teaspoon hot chili powder</ListItem>
+      <ListItem>1 pound large shrimp, shelled and deveined</ListItem>
+      <ListItem>1¼ teaspoons lemon zest (from 1 large lemon)</ListItem>
+    </UnorderedList>
+
+     <Button mt={5}>
+     Add Ingredients to Grocery List      
+     </Button>
+     <br></br>
+     <br></br>
+     
+    </DrawerBody>
+
+  </DrawerContent>
+</Drawer>
+</ButtonGroup>
+</Card>
+      </Box>
+
+
+
+</Flex>
     </>
   );
 }
+
+
